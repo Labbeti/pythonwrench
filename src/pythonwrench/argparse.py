90b0905ee55b73d8b9de6b86b198c516c23347c3
Labbeti <etienne.labbe31@gmail.com>
2025-07-13 02:46:44 +0200

    add:  and  functions to convert any argparse argument to builtin value.

--- src/pythonwrench/argparse.py
+++ src/pythonwrench/argparse.py
@@ -1,11 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import Iterable, List, Optional, Union
+from functools import partial
+from typing import (
+    Any,
+    Callable,
+    Iterable,
+    List,
+    Optional,
+    Type,
+    TypeVar,
+    Union,
+    get_args,
+    get_origin,
+)
+
+from pythonwrench.typing.classes import NoneType
+
+T = TypeVar("T")
+
 
 DEFAULT_TRUE_VALUES = ("True", "t", "yes", "y", "1")
 DEFAULT_FALSE_VALUES = ("False", "f", "no", "n", "0")
 DEFAULT_NONE_VALUES = ("None", "null")
+
+
+def parse_to(
+    target_type: Type[T],
+    *,
+    case_sensitive: bool = False,
+    true_values: Union[str, Iterable[str]] = DEFAULT_TRUE_VALUES,
+    false_values: Union[str, Iterable[str]] = DEFAULT_FALSE_VALUES,
+    none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
+) -> Callable[[str], T]:
+    return partial(
+        str_to_type,
+        target_type=target_type,
+        case_sensitive=case_sensitive,
+        true_values=true_values,
+        false_values=false_values,
+        none_values=none_values,
+    )
+
+
+def str_to_type(
+    x: str,
+    target_type: Type[T],
+    *,
+    case_sensitive: bool = False,
+    true_values: Union[str, Iterable[str]] = DEFAULT_TRUE_VALUES,
+    false_values: Union[str, Iterable[str]] = DEFAULT_FALSE_VALUES,
+    none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
+) -> T:
+    result = _str_to_type_impl(
+        x,
+        target_type,
+        case_sensitive=case_sensitive,
+        true_values=true_values,
+        false_values=false_values,
+    )
+    if isinstance(result, Exception):
+        raise result
+    else:
+        return result
 
 
 def str_to_bool(
@@ -19,19 +76,15 @@ def str_to_bool(
 
     - True values: 'True', 'T', 'yes', 'y', '1'.
     - False values: 'False', 'F', 'no', 'n', '0'.
-    - None values: 'None', 'null'
     - Other raises ValueError.
     """
-    true_values = _sanitize_values(true_values)
-    if _str_in(x, true_values, case_sensitive):
-        return True
-
-    false_values = _sanitize_values(false_values)
-    if _str_in(x, false_values, case_sensitive):
-        return False
-
-    values = tuple(true_values + false_values)
-    raise ValueError(f"Invalid argument '{x}'. (expected one of {values})")
+    return str_to_type(
+        x,
+        bool,
+        case_sensitive=case_sensitive,
+        true_values=true_values,
+        false_values=false_values,
+    )
 
 
 def str_to_none(
@@ -45,12 +98,7 @@ def str_to_none(
     - None values: 'None', 'null'
     - Other raises ValueError.
     """
-    none_values = _sanitize_values(none_values)
-    if _str_in(x, none_values, case_sensitive):
-        return None
-
-    values = tuple(none_values)
-    raise ValueError(f"Invalid argument '{x}'. (expected one of {values})")
+    return str_to_type(x, None, case_sensitive=case_sensitive, none_values=none_values)
 
 
 def str_to_optional_bool(
@@ -68,20 +116,9 @@ def str_to_optional_bool(
     - None values: 'None', 'null'
     - Other raises ValueError.
     """
-    true_values = _sanitize_values(true_values)
-    if _str_in(x, true_values, case_sensitive):
-        return True
-
-    false_values = _sanitize_values(false_values)
-    if _str_in(x, false_values, case_sensitive):
-        return False
-
-    none_values = _sanitize_values(none_values)
-    if _str_in(x, none_values, case_sensitive):
-        return None
-
-    values = tuple(true_values + false_values + none_values)
-    raise ValueError(f"Invalid argument '{x}'. (expected one of {values})")
+    return str_to_type(
+        x, Optional[bool], case_sensitive=case_sensitive, none_values=none_values
+    )
 
 
 def str_to_optional_float(
@@ -91,10 +128,9 @@ def str_to_optional_float(
     none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
 ) -> Optional[float]:
     """Convert string values to optional float safely. Intended for argparse boolean arguments."""
-    none_values = _sanitize_values(none_values)
-    if _str_in(x, none_values, case_sensitive):
-        return None
-    return float(x)
+    return str_to_type(
+        x, Optional[float], case_sensitive=case_sensitive, none_values=none_values
+    )
 
 
 def str_to_optional_int(
@@ -104,10 +140,9 @@ def str_to_optional_int(
     none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
 ) -> Optional[int]:
     """Convert string values to optional int safely. Intended for argparse boolean arguments."""
-    none_values = _sanitize_values(none_values)
-    if _str_in(x, none_values, case_sensitive):
-        return None
-    return int(x)
+    return str_to_type(
+        x, Optional[int], case_sensitive=case_sensitive, none_values=none_values
+    )
 
 
 def str_to_optional_str(
@@ -117,10 +152,126 @@ def str_to_optional_str(
     none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
 ) -> Optional[str]:
     """Convert string values to optional str safely. Intended for argparse boolean arguments."""
+    return str_to_type(
+        x, Optional[str], case_sensitive=case_sensitive, none_values=none_values
+    )
+
+
+def _str_to_type_impl(
+    x: str,
+    target_type: Type[T],
+    *,
+    case_sensitive: bool = False,
+    true_values: Union[str, Iterable[str]] = DEFAULT_TRUE_VALUES,
+    false_values: Union[str, Iterable[str]] = DEFAULT_FALSE_VALUES,
+    none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
+) -> T | Exception:
+    if target_type in (str, int, float, None, NoneType, bool):
+        return _str_to_scalar_impl(
+            x,
+            target_type,
+            case_sensitive=case_sensitive,
+            true_values=true_values,
+            false_values=false_values,
+            none_values=none_values,
+        )
+
+    origin = get_origin(target_type)
+    if target_type.__name__ == "Optional":
+        args = (None,) + get_args(target_type)
+    elif origin.__name__ in ("Union", "UnionType"):
+        args = get_args(target_type)
+    else:
+        raise ValueError(f"Invalid argument {target_type=}. (unsupported type)")
+
+    for arg in args:
+        result = _str_to_type_impl(
+            x,
+            arg,
+            case_sensitive=case_sensitive,
+            true_values=true_values,
+            false_values=false_values,
+        )
+        if not isinstance(result, Exception):
+            return result
+
+    return ValueError(f"Invalid argument {x=} with {target_type=}.")
+
+
+def _str_to_scalar_impl(
+    x: str,
+    target_type: Type[T],
+    *,
+    case_sensitive: bool = False,
+    true_values: Union[str, Iterable[str]] = DEFAULT_TRUE_VALUES,
+    false_values: Union[str, Iterable[str]] = DEFAULT_FALSE_VALUES,
+    none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
+) -> Any:
+    if target_type is str:
+        return x
+    elif target_type is int:
+        try:
+            return int(x)
+        except ValueError as err:
+            return err
+    elif target_type is float:
+        try:
+            return float(x)
+        except ValueError as err:
+            return err
+    elif target_type in (None, NoneType):
+        return _str_to_none_impl(
+            x, case_sensitive=case_sensitive, none_values=none_values
+        )
+    elif target_type is bool:
+        return _str_to_bool_impl(
+            x,
+            case_sensitive=case_sensitive,
+            true_values=true_values,
+            false_values=false_values,
+        )
+    else:
+        raise ValueError(f"Invalid argument {target_type=}. (unsupported type)")
+
+
+def _str_to_bool_impl(
+    x: str,
+    *,
+    case_sensitive: bool = False,
+    true_values: Union[str, Iterable[str]] = DEFAULT_TRUE_VALUES,
+    false_values: Union[str, Iterable[str]] = DEFAULT_FALSE_VALUES,
+) -> bool | Exception:
+    true_values = _sanitize_values(true_values)
+    if _str_in(x, true_values, case_sensitive):
+        return True
+
+    false_values = _sanitize_values(false_values)
+    if _str_in(x, false_values, case_sensitive):
+        return False
+
+    values = tuple(true_values + false_values)
+    err = ValueError(f"Invalid argument '{x}'. (expected one of {values})")
+    return err
+
+
+def _str_to_none_impl(
+    x: str,
+    *,
+    case_sensitive: bool = False,
+    none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
+) -> None | Exception:
+    """Convert string values to None safely. Intended for argparse boolean arguments.
+
+    - None values: 'None', 'null'
+    - Other raises ValueError.
+    """
     none_values = _sanitize_values(none_values)
     if _str_in(x, none_values, case_sensitive):
         return None
-    return x
+
+    values = tuple(none_values)
+    err = ValueError(f"Invalid argument '{x}'. (expected one of {values})")
+    return err
 
 
 def _sanitize_values(values: Union[str, Iterable[str]]) -> List[str]:
