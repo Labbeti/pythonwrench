275a13356d8468928cf01d335395f3f6444dcd23
Labbeti <labbeti.pub@gmail.com>
2025-07-16 23:48:10 +0200

    add: StrEnum unittest.

---
+++ tests/test_enum.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from enum import auto
+from unittest import TestCase
+
+from pythonwrench.enum import StrEnum
+
+
+class TestEnum(TestCase):
+    def test_str_enum(self) -> None:
+        class MyEnum(StrEnum):
+            a = auto()
+            b = auto()
+
+        assert MyEnum.a == "a"
+        assert MyEnum.b == "b"
+        assert MyEnum.from_str("a") == MyEnum.a == "a"
+
+        with self.assertRaises(ValueError):
+            _ = MyEnum.from_str("c")
+
+
+if __name__ == "__main__":
+    unittest.main()
