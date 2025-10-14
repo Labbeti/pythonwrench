25c50d4765c5f8d15231379b1ace47ea61ac94c6
Labbeti <labbeti.pub@gmail.com>
2025-10-14 14:50:37 +0200

    mod: test classic theme for doc.

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "press"
+html_theme = "classic"
 
 html_theme_options = {
     "external_links": [
