c97d855ae24594f8bf5bb5de2f8896f2a2645c9b
Labbeti <labbeti.pub@gmail.com>
2025-11-12 00:00:53 +0100

    add: experimental dataclassdict wrapper.

--- src/pythonwrench/dataclasses.py
+++ src/pythonwrench/dataclasses.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from dataclasses import MISSING
-from typing import Any, Dict
+from dataclasses import MISSING, dataclass
+from typing import Any, Callable, Dict, Optional, Type, TypeVar, overload
 
 from pythonwrench.typing.checks import is_dataclass_instance  # noqa: F401
 from pythonwrench.typing.classes import DataclassInstance
+
+T = TypeVar("T")
 
 
 def get_defaults_values(obj: DataclassInstance) -> Dict[str, Any]:
@@ -21,3 +23,64 @@ def get_defaults_values(obj: DataclassInstance) -> Dict[str, Any]:
             defaults[field.name] = default
 
     return defaults
+
+
+@overload
+def dataclassdict(
+    cls: Type[T],
+    /,
+    *,
+    init: bool = True,
+    repr: bool = True,
+    eq: bool = True,
+    order: bool = False,
+    unsafe_hash: bool = False,
+    frozen: bool = False,
+) -> Type[T]: ...
+
+
+@overload
+def dataclassdict(
+    cls: None = None,
+    /,
+    *,
+    init: bool = True,
+    repr: bool = True,
+    eq: bool = True,
+    order: bool = False,
+    unsafe_hash: bool = False,
+    frozen: bool = False,
+) -> Callable[[Type[T]], Type[T]]: ...
+
+
+def dataclassdict(
+    cls: Optional[Type] = None,
+    /,
+    *,
+    init: bool = True,
+    repr: bool = True,
+    eq: bool = True,
+    order: bool = False,
+    unsafe_hash: bool = False,
+    frozen: bool = False,
+    # match_args: bool=True,
+    # kw_only: bool=False,
+    # slots: bool=False,
+):
+    cls_ = dataclass(
+        cls,
+        init=init,
+        repr=repr,
+        eq=eq,
+        order=order,
+        unsafe_hash=unsafe_hash,
+        frozen=frozen,
+        # match_args=match_args,
+        # kw_only=kw_only,
+        # slots=slots,
+    )
+
+    for attr_name in ("keys", "values", "items", "__len__", "__getitem__"):
+        setattr(cls_, attr_name, getattr(cls_.__dict__, attr_name))
+
+    return cls_
