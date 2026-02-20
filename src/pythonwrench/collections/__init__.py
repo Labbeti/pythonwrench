f6a443cdd4ab3c68bd1a7d902428fe5c4138330e
Labbeti <labbeti.pub@gmail.com>
2026-02-20 16:50:44 +0100

    add: lazy loader in init files.

--- src/pythonwrench/collections/__init__.py
+++ src/pythonwrench/collections/__init__.py
@@ -1,43 +1,100 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .collections import (
-    SizedGenerator,
-    contained,
-    dict_list_to_list_dict,
-    dump_dict,
-    duplicate_list,
-    filter_iterable,
-    find,
-    flat_dict_of_dict,
-    flat_list_of_list,
-    flatten,
-    intersect_lists,
-    list_dict_to_dict_list,
-    recursive_generator,
-    shuffled,
-    sorted_dict,
-    unflat_dict_of_dict,
-    unflat_list_of_list,
-    union_dicts,
-    union_lists,
-    unzip,
-)
-from .prop import (
-    all_eq,
-    all_ne,
-    is_full,
-    is_sorted,
-    is_unique,
-)
-from .reducers import (
-    intersect,
-    prod,
-    reduce_add,
-    reduce_and,
-    reduce_matmul,
-    reduce_mul,
-    reduce_or,
-    sum,
-    union,
-)
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .collections import (
+        SizedGenerator,
+        contained,
+        dict_list_to_list_dict,
+        dump_dict,
+        duplicate_list,
+        filter_iterable,
+        find,
+        flat_dict_of_dict,
+        flat_list_of_list,
+        flatten,
+        intersect_lists,
+        list_dict_to_dict_list,
+        recursive_generator,
+        shuffled,
+        sorted_dict,
+        unflat_dict_of_dict,
+        unflat_list_of_list,
+        union_dicts,
+        union_lists,
+        unzip,
+    )
+    from .prop import (
+        all_eq,
+        all_ne,
+        is_full,
+        is_sorted,
+        is_unique,
+    )
+    from .reducers import (
+        intersect,
+        prod,
+        reduce_add,
+        reduce_and,
+        reduce_matmul,
+        reduce_mul,
+        reduce_or,
+        sum,
+        union,
+    )
+
+else:
+    import lazy_loader as lazy
+
+    __getattr__, __dir__, __all__ = lazy.attach(
+        __name__,
+        submodules=[
+            "collections",
+            "prop",
+            "reducers",
+        ],
+        submod_attrs={
+            "collections": [
+                "SizedGenerator",
+                "contained",
+                "dict_list_to_list_dict",
+                "dump_dict",
+                "duplicate_list",
+                "filter_iterable",
+                "find",
+                "flat_dict_of_dict",
+                "flat_list_of_list",
+                "flatten",
+                "intersect_lists",
+                "list_dict_to_dict_list",
+                "recursive_generator",
+                "shuffled",
+                "sorted_dict",
+                "unflat_dict_of_dict",
+                "unflat_list_of_list",
+                "union_dicts",
+                "union_lists",
+                "unzip",
+            ],
+            "prop": [
+                "all_eq",
+                "all_ne",
+                "is_full",
+                "is_sorted",
+                "is_unique",
+            ],
+            "reducers": [
+                "intersect",
+                "prod",
+                "reduce_add",
+                "reduce_and",
+                "reduce_matmul",
+                "reduce_mul",
+                "reduce_or",
+                "sum",
+                "union",
+            ],
+        },
+    )
